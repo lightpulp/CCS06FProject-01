911bf9712c132ca760b7801b4ea10a58b7a5a0a9
prgcalma <calma.paulrichard@gmail.com>
2025-05-15 16:51:16 +0800

    added jquery validate script

--- frontend/page_register.php
+++ frontend/page_register.php
@@ -102,11 +102,8 @@
     </div>
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.21.0/jquery.validate.min.js" integrity="sha512-KFHXdr2oObHKI9w4Hv1XPKc898mE4kgYx58oqsc/JqqdLMDI4YjOLzom+EMlW8HFUd0QfjfAvxSL6sEq/a42fQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="../backend/javascript/register.js">
-        
-</script>
-
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="../assets/script/script.js"></script>
 </body>
 </html>
